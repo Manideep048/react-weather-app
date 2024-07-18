d4674495678ba152b4e1dc9cd77113c3ce2e0ad7
Manideep <211fa04630@vignan.ac.in>
2024-07-19 02:40:33 +0530

    Created weather app for the first tym

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const GeoApiOptions = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '781e468346msh5ae59889573b8a6p1b2875jsn7617bce86f9d',
+      'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  
+  export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+  export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+  export const WEATHER_API_KEY = 'd26de8082407122f607a523292e07bb1';
+  
---
+++ src/components/search/search.js
@@ -0,0 +1,44 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { GEO_API_URL, GeoApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        GeoApiOptions
+      );
+      const data = await response.json();
+
+      const options = data.data.map((city) => ({
+        value: `${city.latitude} ${city.longitude}`,
+        label: `${city.name}, ${city.countryCode}`,
+      }));
+
+      return { options };
+    } catch (err) {
+      console.error(err);
+      return { options: [] };
+    }
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
